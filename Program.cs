8da75e75a33f496dcd7c654c65c3b14a34811d7d
estebancalabria <esteban.calabria@gmail.com>
2021-10-15 08:05:23 -0300

    Commit Inicial

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sonar_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int b = 2;
+            int a = 2;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
